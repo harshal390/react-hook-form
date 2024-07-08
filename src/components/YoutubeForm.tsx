4332ebf99ccbee4f26764ed5d57ee811c8cd87c8
Harshal Kahar <harshal.kahar@esparkbizmail.com>
2024-07-08 11:31:33 +0530

    error messages

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -11,7 +11,8 @@ type formValues = {
 const YoutubeForm = () => {
 
     const form = useForm<formValues>();
-    const { register, control, handleSubmit } = form;
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
     const onSubmit = (data: formValues) => {
         console.log("form submitted", data);
     }
@@ -20,9 +21,9 @@ const YoutubeForm = () => {
         <div className="flex flex-col gap-5">
             <div className="text-4xl font-semibold">Youtube Form {renderCount / 2} </div>
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5 items-center" noValidate>
-                <Input labelValue="username" type="text" id="username" name="username" register={register} />
-                <Input labelValue="email" type="text" id="email" name="email" register={register} />
-                <Input labelValue="channel" type="text" id="channel" name="channel" register={register} />
+                <Input labelValue="username" type="text" id="username" name="username" register={register} errors={errors}/>
+                <Input labelValue="email" type="text" id="email" name="email" register={register} errors={errors}/>
+                <Input labelValue="channel" type="text" id="channel" name="channel" register={register} errors={errors}/>
                 <button className="px-10 py-2 border rounded-lg text-center w-fit">Submit</button>
             </form>
             <DevTool control={control} /> {/* set up the dev tool */}
--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,9 +1,9 @@
-const Input = ({ labelValue, type, name, id, register }: any) => {
+const Input = ({ labelValue, type, name, id, register, errors }: any) => {
 
     const validation: any = {
         username: { required: "Username is required" },
         email: {
-            required: "Required",
+            required: "Email is Required",
             pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 message: "invalid email address"
@@ -14,9 +14,11 @@ const Input = ({ labelValue, type, name, id, register }: any) => {
         }
     }
     return (
-        <div className="grid grid-cols-12 gap-5 items-center w-full">
+        <div className="grid grid-cols-12 gap-x-5 gap-y-2 items-center w-full">
             <label htmlFor={labelValue} className="capitalize col-span-1 text-start">{labelValue}</label>
             <input type={type} name={name} id={id} {...register(name, validation[name])} className="border-0 outline-none rounded-lg p-2 col-span-11" />
+            <div className="col-span-1"></div>
+            <p className="col-span-11 text-sm text-red-600 capitalize text-start">{errors[name]?.message}</p>
         </div>
     )
 }
